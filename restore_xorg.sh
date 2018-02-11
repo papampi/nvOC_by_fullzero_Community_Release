b7e88d1511721eb366fa298778ef87a7d312de78
Payam Nab <papampi@users.noreply.github.com>
2018-02-11 13:54:45 +0330

    Update restore_xorg.sh

--- restore_xorg.sh
+++ restore_xorg.sh
@@ -9,6 +9,7 @@ else
   sudo wget -N https://raw.githubusercontent.com/papampi/nvOC_by_fullzero_Community_Release/19-2.1/xorg.conf -O /etc/X11/xorg.conf.default
   echo "Restore default xorg.conf"
   sudo cp '/etc/X11/xorg.conf.default' '/etc/X11/xorg.conf'
+  sudo cp '/etc/X11/xorg.conf.default' '/etc/X11/xorg.conf.backup'
 fi
 echo "Default /etc/X11/xorg.conf restored, reboot in 5 seconds"
 sleep 5
